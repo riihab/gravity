a2231b70fa0ce53a84c2693157c3748c8c25ff4f
RIHAB MEHBOOB <Rihab@shihabs-macbook-air.local>
2018-07-06 18:08:12 +0100

    Initial Commit

---
+++ gravity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gravity
+//
+//  Created by Rihab Mehboob on 06/07/2018.
+//  Copyright © 2018 Rihab Mehboob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
